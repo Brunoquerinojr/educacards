f5ad8dc54107d3c5897e628848c22349b6ec46bf
EduENL <130664287+EduENL@users.noreply.github.com>
2024-05-10 08:52:40 -0300

    BRUNO_NAO_MEXE_finalizado

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import './nabar.css'
-<<<<<<< HEAD
-import Logo from './img/Header.jpg'
-import Seta from './img/Seta.png'
-=======
 import Logo from './img/logo.png'
->>>>>>> afb9f988f28fc63bf90e9b5357dbf0454c3e5a86
 
 const NavBar = () => {
   return (
     <nav>
         <ul>
-<<<<<<< HEAD
-            <li><a href=""><img src={Seta} alt="Seta de Voltar" /></a></li>
-=======
             <li></li>
->>>>>>> afb9f988f28fc63bf90e9b5357dbf0454c3e5a86
             <li><h1>Bem-Vindo(a)</h1></li>
             <li><a href=""><img src={Logo} alt="Logo" /></a></li>
         </ul>
